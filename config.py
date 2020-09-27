1078e745f70b267d97467c50d7686f3e4517581f
kelvin-daniel <kaymutor@gmail.com>
2020-09-27 14:19:27 +0300

    initialize database

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevon:37042490@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
     DEBUG = True
