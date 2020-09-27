4c9a0b0753758d28858fd1791a3fe190f2bf7bb8
kelvin-daniel <kaymutor@gmail.com>
2020-09-27 14:44:50 +0300

    get the app to run locally

--- app/request.py
+++ app/request.py
@@ -1,7 +1,7 @@
-import urllib.request,json
+import json,requests
 
 def get_quotes():
-    response = urllib.request.get('http://quotes.stormconsultancy.co.uk/random.json')
+    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
     if response.status_code == 200:
         quote = response.json()
         return quote
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ SECRET_KEY ='the_key'
 DATABASE_URL ='postgresql+psycopg2://kevon:37042490@localhost/blog'
 MAIL_PASSWORD ='37042490'
 
-python manage.py serve
+python manage.py run
