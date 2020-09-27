86437ba29ef5caf25e98f7019f72da1823878127
kelvin-daniel <kaymutor@gmail.com>
2020-09-27 17:38:22 +0300

    configure simple mde

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
+from flask_simplemde import SimpleMDE
 
 db = SQLAlchemy()
 mail = Mail()
@@ -13,6 +14,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
 
 
 
@@ -30,5 +32,6 @@ def create_app():
     bootstap.init_app(app)
     configure_uploads(app,photos)
     mail.init_app(app)
+    simple.init_app(app)
 
     return app
