6bd0e337259edc9658a34d596bcc522378c01bd5
kelvin-daniel <kaymutor@gmail.com>
2020-09-27 19:58:10 +0300

    add profile updater

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,7 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import IMAGES, UploadSet,configure_uploads
-from flask_simplemde import SimpleMDE
+from flask_uploads import IMAGES,UploadSet,configure_uploads
 
 db = SQLAlchemy()
 mail = Mail()
@@ -14,7 +13,6 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
-simple = SimpleMDE()
 
 
 
@@ -32,6 +30,5 @@ def create_app():
     bootstap.init_app(app)
     configure_uploads(app,photos)
     mail.init_app(app)
-    simple.init_app(app)
 
     return app
